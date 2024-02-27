39f6abcacbe99af769b92d78aba41d9c22b0b6fa
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-02-26 20:52:25 -0600

    updated Models and Seed Data

--- Models/PaymentType.cs
+++ Models/PaymentType.cs
@@ -3,7 +3,7 @@
     public class PaymentType
     {
         public int Id { get; set; }
-        public string? UserId { get; set; }
+        public int? UserId { get; set; }
         public string? Type { get; set; }
     }
 }
